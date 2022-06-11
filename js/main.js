b394590c0673755445fc4c2d83f748e43b4546f6
Ray Otsuka <56874556+ray-o-0509@users.noreply.github.com>
2022-06-11 23:42:19 +0900

    [upload] upload data

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function scroll_to(num) {
+    changeMenu();
+    $("html,body").animate({
+        scrollTop: $('.' + num).offset().top - 100,
+    });
+}
+
+$(window).scroll(function() {
+    var scroll = $(this).scrollTop();
+    const maxInt = $('.get_height').innerHeight() * 0.7;
+    if (scroll < maxInt) {
+        $('.background_img').css('opacity', 1 - (0.7 * scroll / maxInt));
+        $('.background_img_sm').css('opacity', 1 - (0.7 * scroll / maxInt));
+
+    } else {
+        $('.background_img').css('opacity', '.3');
+        $('.background_img_sm').css('opacity', '.3');
+    }
+});
+
+let menu_opened = false;
+
+function changeMenu() {
+    menu_opened = !menu_opened;
+    let _text;
+    if (location.href.indexOf('/en/') < 0) {
+        _text = 'img';
+    } else {
+        _text = '../img'
+    }
+    if (menu_opened) {
+        $('.menu_icon').attr('src', _text + '/menu_close.svg');
+        $('.menu_open').css('height', '300px');
+    } else {
+        $('.menu_icon').attr('src', _text + '/menu_open.svg');
+        $('.menu_open').css('height', '0px');
+    }
+}
